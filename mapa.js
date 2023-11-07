e690fc72c226caa91af7422c09dd7c401219c1cf
Rodrigo Braz <orodrigobraz18@gmail.com>
2023-11-06 22:14:55 -0300

    Refatoração

--- mapa.js
+++ mapa.js
@@ -1,44 +1,52 @@
-var map = L.map('map').setView([-22.5514, -45.7792], 14);
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+const map = L.map('map').setView([-22.5514, -45.7792], 14);
 
-            var marker;
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-            function pressionarEnter(event) {
-                if (event.keyCode === 13) {
-                    buscarLocalizacao();
-                }
+let marker;
+
+/**
+ * 
+ * @param {*} event Evento ao pressionar qualquer tecla
+ */
+const pressionarEnter = (event) => {
+    if (event.keyCode === 13) {
+        buscarLocalizacao();
+    }
+}
+
+/**
+ * Busca a localização com base no nome digitado dentro do input
+ * 
+ */
+const buscarLocalizacao = async () => {
+    const nomeLocalizacao = document.getElementById('localizacao').value;
+    if (nomeLocalizacao === '') {
+        alert('Por favor, digite a localização.');
+        return;
+    }
+
+    const url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(nomeLocalizacao);
+
+    try {
+        const dados = await (await fetch(url)).json();
+
+        if (dados.length === 0) {
+            alert('Localização não encontrada.');
+        } else {
+            const latitude = parseFloat(dados[0].lat);
+            const longitude = parseFloat(dados[0].lon);
+
+            if (marker) {
+                map.removeLayer(marker);
             }
 
-            function buscarLocalizacao() {
-                var nomeLocalizacao = document.getElementById('localizacao').value;
-                if (nomeLocalizacao === '') {
-                    alert('Por favor, digite a localização.');
-                    return;
-                }
+            marker = L.marker([latitude, longitude]).addTo(map);
 
-                var url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(nomeLocalizacao);
-
-                fetch(url)
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.length === 0) {
-                            alert('Localização não encontrada.');
-                        } else {
-                            var latitude = parseFloat(data[0].lat);
-                            var longitude = parseFloat(data[0].lon);
-
-                            if (marker) {
-                                map.removeLayer(marker);
-                            }
-
-                            marker = L.marker([latitude, longitude]).addTo(map);
-
-                            map.setView([latitude, longitude], 13);
-                        }
-                    })
-                    .catch(error => {
-                        alert('Ocorreu um erro ao buscar a localização: ' + error);
-                    });
-            }
+            map.setView([latitude, longitude], 13);
+        }
+    } catch (error) {
+        alert('Ocorreu um erro ao buscar a localização: ' + error);
+    }
+}
